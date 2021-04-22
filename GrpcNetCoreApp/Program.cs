21346bb665437f64fc9d4b128ae36a2e814cb46b
patrikduch <duchpatrik@icloud.com>
2021-04-22 13:14:53 +0200

    Initial commit

---
+++ GrpcNetCoreApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcNetCoreServiceApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest() 
+            { 
+                Name = "Patrik Duch" 
+            });
+            Console.WriteLine(response.Message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcNetCoreService/Services/GreeterService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace GrpcNetCoreService.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+
+        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return new HelloReply()
+            {
+                Message = "Bc. " + request.Name
+            };
+        }
+
+    }
+}
